3e551bfcf0e4e90868079ee69dff4af3e0a49cfd
Maksim Khrestinin <maksim.khrestinin@gmail.com>
2023-04-09 21:58:56 +0400

    Added methods for working with URIs

--- src/main/java/org/example/PeopleGenerator.java
+++ src/main/java/org/example/PeopleGenerator.java
@@ -1,4 +1,23 @@
 package org.example;
 
+import org.apache.http.client.fluent.Content;
+import org.apache.http.client.fluent.Request;
+
+import java.io.IOException;
+
 public class PeopleGenerator {
+
+    private String t = "https://api.randomdatatools.ru/?typeName=all&unescaped=false&params=LastName,FirstName,FatherName,YearsOld,Gender,DateOfBirth,City,Address&count=";
+
+    public void modifyURI(int n) { t += n; }
+    public String returnJson(String uri) throws IOException {
+        Content getResult = Request.Get(uri).execute().returnContent();
+        return getResult.asString();
+    }
+
+    public void printPeople(String json) {
+
+    }
+
+
 }

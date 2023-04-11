12f33dcec0211117f119751b559e820c688a5ba9
Maksim Khrestinin <maksim.khrestinin@gmail.com>
2023-04-11 13:37:13 +0400

    Аdded font file

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 public class App {
 
     public static void main( String[] args ) throws IOException {
-        WorkingAPI.setQuantity(10);
+       /* WorkingAPI.setQuantity(10);
         ProcessorJSON p = new ProcessorJSON();
         PeopleGenerator.printPeople(p.SplitData(p.modifyStructure(WorkingAPI.returnJSONResponse(WorkingAPI.getMainURI()))));
+        */
+        RecordPDF.createFile(9);
     }
 }
--- src/main/java/org/example/RecordPDF.java
+++ src/main/java/org/example/RecordPDF.java
@@ -1,4 +1,61 @@
 package org.example;
 
+import org.apache.pdfbox.contentstream.PDContentStream;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.*;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
 public class RecordPDF {
+
+    public static void createFile(int n) throws IOException {
+        PDDocument document = new PDDocument();
+        PDPage page = new PDPage();
+        document.addPage(page);
+
+        int pageHeight = (int) page.getCropBox().getHeight();
+        int pageWidth = (int) page.getCropBox().getWidth();
+
+        PDPageContentStream contentStream = new PDPageContentStream(document, page);
+
+        contentStream.setStrokingColor(Color.DARK_GRAY);
+        contentStream.setLineWidth(0.7f);
+
+        int initX = 20;
+        int initY = pageHeight - 10;
+        int cellHeight = 6;
+        int cellWidth = 36;
+
+        int rowCount = n+1;
+        int colCount = 14;
+
+        PDFont pdfFont = PDType0Font.load(document, new File("D:\\PDF generator\\PDF-generator\\ArialMT.ttf"));
+
+        for(int i = 1; i <= rowCount; i++) {
+            for(int j = 1; j <= colCount; j++) {
+                contentStream.addRect(initX, initY, cellWidth, -cellHeight);
+
+                contentStream.beginText();
+                contentStream.newLineAtOffset(initX+5, initY-cellHeight+2);
+                contentStream.setFont(pdfFont, 4);
+                contentStream.showText("Информатика");
+                contentStream.endText();
+
+                initX += cellWidth;
+            }
+            initX = 20;
+            initY -= cellHeight;
+        }
+
+        contentStream.stroke();
+        contentStream.close();
+
+        document.save("D:\\PDF generator\\mypdf.pdf");
+        document.close();
+        System.out.println("PDF created");
+    }
 }
